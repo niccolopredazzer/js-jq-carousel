f18b4fb7849449f769893e4bc5235f2819c25172
Niccolò Predazzer <niccolo.predazzer@gmail.com>
2020-03-02 14:55:46 +0100

    start

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+    // Invoco le funzioni per cambiare slide
+    $('.next').click(nextSlide);        // Al click richiamo la funzione per aggiornare l'immagine visualizzando la successiva
+
+
+    // Definisco le funzioni nextImage e prevImage
+    function nextSlide() {
+        if ( $('.images img.active').hasClass('last') ) {
+
+            $('.images img.active').removeClass('active');
+            $('.images img.first').addClass('active');
+
+            // Pallini
+            $('.slider-nav i.active').removeClass('active');
+            $('.slider-nav i.first').addClass('active');
+
+        } else {
+            var imgAttiva = $('.images img.active'); // Rimuovo l'active
+            var prossimaImg = $('.images img.active').next();
+
+            imgAttiva.removeClass('active');
+            prossimaImg.addClass('active');
+
+            // Pallini
+            var pallinoAttivo = $('.slider-nav i.active'); // Rimuovo l'active
+            var prossimaPallino = $('.slider-nav i.active').next();
+
+            pallinoAttivo.removeClass('active');
+            prossimaPallino.addClass('active');
+        }
+    }
+
+
+
+
+});
